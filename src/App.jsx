5c81e9a35be257554f91ad8ed396f38bbe8c1d8e
Fateme <76264392+fatememajdi@users.noreply.github.com>
2024-08-04 10:35:05 +0330

    [feat] add input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
+import Input from './components/input/input';
 
 export default function App() {
-	return <div>App</div>;
+	return <div>
+		sd
+		<Input title={'Username'} type={'text'} name={'username'} />
+
+	</div>;
 }
