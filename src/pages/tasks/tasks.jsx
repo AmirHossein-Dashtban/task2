0443293bdc6a356e6264ce7bd9d08d8979d42374
AmirHossein-Dashtban <amirhossein.namaghi@gmail.com>
2024-08-05 10:40:10 +0330

    [feat] complete "tasklist1" page

--- src/pages/tasks/tasks.jsx
+++ src/pages/tasks/tasks.jsx
@@ -1,5 +1,40 @@
-const Tasks = () => {
-    return <div>Tasks page</div>
-};
+import React, { useState } from 'react';
+import Box from '../../components/box-component/Box';
+import BoxHeader from '../../components/box-header/BoxHeader';
+import TaskListContainer from '../../components/tasklist-container/TaskListContainer';
+import Pagination from '../../components/pagination/Pagination';
+import Button from '../../components/button/button';
+import { LogOutIcon, Plus } from '../../assets/icons/index';
 
-export default Tasks;
+export default function tasks() {
+	const [tasks, setTasks] = useState([
+		{ taskName: 'Task #1' },
+		{ taskName: 'Task #2' },
+		{ taskName: 'Task #3' },
+	]);
+
+	return (
+		<section
+			className="page-container"
+			style={{
+				alignItems: 'center',
+				display: 'flex',
+				justifyContent: 'center',
+				width: '100%',
+			}}
+		>
+			<Box>
+				<BoxHeader
+					leftIcon={<LogOutIcon />}
+					headingText={`Jenny's Task`}
+				></BoxHeader>
+
+				<TaskListContainer tasks={tasks}></TaskListContainer>
+
+				<Pagination />
+
+				<Button text={`Task`} icon={<Plus />} />
+			</Box>
+		</section>
+	);
+}
