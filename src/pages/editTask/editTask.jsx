f10ca04d12500a10e66f0178e74ce30c83a229ac
Fateme <76264392+fatememajdi@users.noreply.github.com>
2024-08-05 11:00:57 +0330

    [feat] add edit task page

--- src/pages/editTask/editTask.jsx
+++ src/pages/editTask/editTask.jsx
@@ -1,5 +1,24 @@
+import './edittask.css';
+import Box from '../../components/box-component/Box';
+import BoxHeader from '../../components/box-header/BoxHeader';
+import Input from '../../components/input/input';
+import Button from '../../components/button/button';
+import { ArrowRightIcon, Trash } from '../../assets/icons';
+
 const EditTak = () => {
-    return <div>edit task page</div>
+    return <div style={{ display: 'flex', height: '100vh', width: '100vw' }}>
+        <Box>
+            <BoxHeader headingText={'Edit Task #1'} rightIcon={<ArrowRightIcon />} leftIcon={<Trash />} />
+
+            <div className='inputs-container'>
+                <Input type={'text'} name={'name'} title={'name'} />
+                <Input type={'text'} name={'priority'} title={'priority'} className={'priority-input'} />
+            </div>
+
+            <Button text={'Save'} type={'sumit'} />
+        </Box>
+
+    </div>
 };
 
 export default EditTak;
