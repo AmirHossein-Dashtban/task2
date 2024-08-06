0fa1b8e929a7a2f48d256a4d5ad368e410125181
AmirHossein-Dashtban <amirhossein.namaghi@gmail.com>
2024-08-06 22:30:19 +0330

    [feat] complete login logic

--- src/pages/tasks/tasks.jsx
+++ src/pages/tasks/tasks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '../../components/box-component/Box';
 import BoxHeader from '../../components/box-header/BoxHeader';
 import TaskListContainer from '../../components/tasklist-container/TaskListContainer';
@@ -6,16 +6,37 @@ import Pagination from '../../components/pagination/Pagination';
 import Button from '../../components/button/button';
 import { LogOutIcon, Plus } from '../../assets/icons/index';
 import PageContainer from '../../components/page-container/page-container';
+import { StateContext } from '../../data/data';
+import { useContext } from 'react';
 
 export default function tasks() {
+	const [userInfo, setUserInfo] = useState({
+		id: '',
+		userName: '',
+		password: '',
+	});
 	const [tasks, setTasks] = useState([]);
+	const states = useContext(StateContext);
+	const userTasks = states[1].tasks;
+
+	useEffect(() => {
+		const info = { ...JSON.parse(localStorage.getItem('userInfo')) };
+		const currentUserTasks = userTasks.filter(
+			(task) => task.userID === info.id
+		);
+		setUserInfo(info);
+		setTasks([...currentUserTasks]);
+		console.log(userInfo);
+	}, []);
+	console.log(userInfo);
 
 	return (
 		<PageContainer>
 			<Box>
+				{/* {console.log(userInfo.userName)} */}
 				<BoxHeader
 					leftIcon={[<LogOutIcon />, '/login']}
-					headingText={`Jenny's Task`}
+					headingText={`${userInfo.userName}'s Tasks`}
 				></BoxHeader>
 
 				<TaskListContainer tasks={tasks}></TaskListContainer>
