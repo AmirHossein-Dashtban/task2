a3159daaa0f5b5b14d6c306b2ffa55617edb5b50
Fateme <76264392+fatememajdi@users.noreply.github.com>
2024-08-06 12:02:59 +0330

    [fix] add formik for inputs value

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,10 +1,10 @@
 import './input.css'
 
-export default function Input({ title, name, type, placeHolder, icon, className }) {
+export default function Input({ title, name, type, placeHolder, icon, className, onChange, onBlur, value }) {
     return <div className={'input-card ' + (className ? className : "")}>
         <div className='main-section'>
             <label>{title}</label>
-            <input name={name} type={type} placeholder={placeHolder ? placeHolder : ''} />
+            <input name={name} type={type} placeholder={placeHolder ? placeHolder : ''} onChange={onChange} onBlur={onBlur} value={value} />
         </div>
         {
             icon &&
--- src/pages/createTask/createTak.jsx
+++ src/pages/createTask/createTak.jsx
@@ -4,21 +4,55 @@ import BoxHeader from '../../components/box-header/BoxHeader';
 import Input from '../../components/input/input';
 import Button from '../../components/button/button';
 import PageContainer from '../../components/page-container/page-container';
+import { Formik } from 'formik';
+import { useNavigate } from 'react-router-dom';
 
 const CreateTask = () => {
+    const navigation = useNavigate();
+
     return <PageContainer>
-        <Box>
-            <BoxHeader headingText={'Task Manager'} />
+        <Formik
+            initialValues={{ name: '', priority: '' }}
+            onSubmit={(values, { setSubmitting }) => {
+                navigation('/list');
+            }}
+        >
+            {({
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                values
+            }) => (
+                <form onSubmit={handleSubmit}>
+                    <Box>
+                        <BoxHeader headingText={'Task Manager'} />
 
-            <div className='inputs-container'>
-                <Input type={'text'} name={'name'} title={'name'} />
-                <Input type={'text'} name={'priority'} title={'priority'} className={'priority-input'} />
-            </div>
+                        <div className='inputs-container'>
+                            <Input
+                                type={'text'}
+                                name={'name'}
+                                title={'name'}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.name}
+                            />
+                            <Input
+                                type={'text'}
+                                name={'priority'}
+                                title={'priority'}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.priority}
+                                className={'priority-input'} />
+                        </div>
 
-            <Button text={'Create'} type={'sumit'} />
-        </Box>
+                        <Button text={'Create'} type={'sumit'} />
+                    </Box>
 
-    </PageContainer>
+                </form>
+            )}
+        </Formik>
+    </PageContainer >
 };
 
 export default CreateTask;
