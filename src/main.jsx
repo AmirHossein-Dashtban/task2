61a02a24c315fb839127d3c04219428a3da9cddf
AmirHossein-Dashtban <amirhossein.namaghi@gmail.com>
2024-08-13 11:44:31 +0330

    [refactor] provide a provider for main.jsx to use redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import PocketBaseProvider from './context/pocketbase/PocketBaseProvider';
 import App from './App.jsx';
+import { store } from './app/store.js';
+import { Provider } from 'react-redux';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<PocketBaseProvider>
-			<App />
-		</PocketBaseProvider>
+		<Provider store={store}>
+			<PocketBaseProvider>
+				<App />
+			</PocketBaseProvider>
+		</Provider>
 	</React.StrictMode>
 );
